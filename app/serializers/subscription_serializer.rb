cacca24f0ca760a2339c1f309d88fe4cf6f808d5
Felix Kipruto <kimosopfelix@gmail.com>
2023-05-02 00:57:46 +0300

    deleted serializers

--- app/serializers/subscription_serializer.rb
+++
@@ -1,5 +0,0 @@
-# class SubscriptionSerializer < ActiveModel::Serializer
-#   attributes :id
-#   belongs_to :category
-#   belongs_to :user
-# end
--- app/serializers/wishlist_serializer.rb
+++
@@ -1,8 +0,0 @@
-# class WishlistSerializer < ActiveModel::Serializer
-#     attributes :id, :content_id, :user_id, :created_at, :content_details
-  
-#     def content_details
-#       ContentSerializer.new(object.content).as_json if object.content.present?
-#     end
-#   end
-  
